4622226f5b62ac6704dcf7b750a1ca61f6966c8d
aalibrahimi <166450703+aalibrahimi@users.noreply.github.com>
2025-04-25 10:24:01 -0700

    baank change

--- src/MyComponents/HomeDashboard/companyStats.tsx
+++ src/MyComponents/HomeDashboard/companyStats.tsx
@@ -54,7 +54,7 @@ const CompanyStats = () => {
         <StatCard
           icon={CircleDollarSign}
           label="Revenue"
-          value="$887.69"
+          value="$989.00"
         />
         <StatCard icon={BarChart3} label="Subscription" value="$46" />
         <StatCard icon={Boxes} label="Active Bots" value="1" />
