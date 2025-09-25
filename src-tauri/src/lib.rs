644ad050ffe8e542e238f581daa4a2a078b240dd
blazehp <blazehunterhp@gmail.com>
2025-09-26 01:19:06 +0200

    Added window-state saver ( tauri plugin ). Now app remember size and position of app window from previous session

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -196,6 +196,7 @@ async fn send_invoice(
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             github_webhooks::get_github_webhooks
